57cb374d235f395592610c3cc8caf3700a37bb95
Vitaliy Petroff <petroff@mail.ru>
2025-05-18 17:47:04 +0100

    dinamic locale change 05

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,51 +1,50 @@
-import {reactive, watch} from 'vue';
-
+import { reactive, watch } from "vue";
 
 /** @param {String} key */
 export function useStorage(key) {
-    let storedValues = localStorage.getItem(key + '_storage');
+  let storedValues = localStorage.getItem(key + "_storage");
 
-    const storage = reactive(JSON.parse(storedValues ?? '{}'));
+  const storage = reactive(JSON.parse(storedValues ?? "{}"));
 
-    watch(storage, setItem);
+  watch(storage, setItem);
 
-    function setItem() {
-        if (!Object.keys(storage).length) {
-            localStorage.removeItem(key + '_storage');
-        } else {
-            localStorage.setItem(key + '_storage', JSON.stringify(storage));
-        }
+  function setItem() {
+    if (!Object.keys(storage).length) {
+      localStorage.removeItem(key + "_storage");
+    } else {
+      localStorage.setItem(key + "_storage", JSON.stringify(storage));
     }
+  }
 
-    /**
-     * @param {String} key
-     * @param {*} value
-     */
-    function setStore(key, value) {
-        storage[key] = value;
-    }
+  /**
+   * @param {String} key
+   * @param {*} value
+   */
+  function setStore(key, value) {
+    storage[key] = value;
+  }
 
-    /**
-     * @param {String} key
-     */
-    function removeStore(key) {
-        delete storage[key];
-    }
+  /**
+   * @param {String} key
+   */
+  function removeStore(key) {
+    delete storage[key];
+  }
 
-    function clearStore() {
-        Object.keys(storage).map(key => removeStore(key));
-    }
+  function clearStore() {
+    Object.keys(storage).map((key) => removeStore(key));
+  }
 
-    /**
-     * @param {String} key
-     * @param {*} defaultValue
-     */
-    const getStore = (key, defaultValue = null) => {
-        if (storage.hasOwnProperty(key)) {
-            return storage[key]
-        }
-        return defaultValue;
-    }
+  /**
+   * @param {String} key
+   * @param {*} defaultValue
+   */
+  const getStore = (key, defaultValue = null) => {
+    // if (storage.hasOwnProperty(key)) {
+    //     return storage[key]
+    // }
+    return defaultValue;
+  };
 
-    return {getStore, setStore, removeStore, clearStore};
+  return { getStore, setStore, removeStore, clearStore };
 }
